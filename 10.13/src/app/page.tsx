780a60c062cb4188f94223acd8c93d96ab3496d8
songfy <songfydesign@gmail.com>
2024-10-13 10:34:22 +0200

    10.13

---
+++ 10.13/src/app/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+//react在处理html的时候不支持html和while -> 但是你可以自定义一些function去处理 数据2
+//for while难的是算法和嵌套
+
+// 这样可以
+// function add(){
+//   while(){
+
+//   }
+//   for(){
+
+//  }
+// }
+
+//.map -> for的效果
+
+const arr1 = Array(7).fill(0); //创造一个长度为7  被0填充的数组
+
+export default function Home() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
--- homepage/src/app/proto/page.tsx
+++ homepage/src/app/proto/page.tsx
@@ -1,5 +1,11 @@
+import { CardWithForm } from "@/components/CardWithForm";
 import React from "react";
 
 export default function Proto() {
-  return <div>Proto</div>;
+  return (
+    <div className="flex flex-col space-y-4">
+      <div>Proto</div>
+      <CardWithForm />
+    </div>
+  );
 }
